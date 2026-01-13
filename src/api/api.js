55955eca445d41deec618d26482d3c08bfdeb90f
Hinduswaraj <hinduswarajyouth@gmail.com>
2026-01-14 01:43:47 +0530

    member

--- src/api/api.js
+++ src/api/api.js
@@ -7,6 +7,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache"
   },
   withCredentials: true,
 });
